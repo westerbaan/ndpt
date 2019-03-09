b2be54814788e55a930af7b9f1ce0bf95a905c2b
Bram Westerbaan <bram@westerbaan.name>
2019-03-09 15:43:19 +0100

    scene tweak

--- ndpt.cpp
+++ ndpt.cpp
@@ -108,7 +108,7 @@ public:
   template <typename T> Vec<S, N>(const std::initializer_list<T> &l) {
     // For some reason clang doesn't think l.size() is constexpr
     // static_assert(l.size() < N, "initializer_list too long");
-    assert(l.size() < N);
+    assert(l.size() <= N);
     std::copy(l.begin(), l.end(), this->vals.begin());
     std::fill(&this->vals[l.size()], this->vals.end(), 0);
   }
@@ -755,7 +755,7 @@ private:
 };
 
 template <typename S, size_t N> void render(size_t nWorkers) {
-  constexpr int dpi = 150;
+  constexpr int dpi = 600;
   constexpr int hRes = static_cast<int>(6.81102 * static_cast<double>(dpi));
 
   // Ratio of front cover is 173 : 246
@@ -767,20 +767,20 @@ template <typename S, size_t N> void render(size_t nWorkers) {
   Vec<S, N> origin{0};
   Vec<S, N> e0{1};
 
-  // ReflectiveSphere<S,N> sphere(origin, .9);
-  ReflectiveTorus<S, N> torus(origin, .636);
+  ReflectiveSphere<S,N> sphere(origin, .9);
+  //ReflectiveTorus<S, N> torus(origin, .636);
   std::array<Vec<S, N>, N> floorAxes;
   for (size_t i = 0; i < N; i++)
     for (size_t j = 0; j < N; j++)
       floorAxes[i][j] = (j == i + 1) ? 1 : 0;
   HyperCheckerboard<S, N> floor(Ray<S, N>(e0, (-e0).normalize()), floorAxes);
-  HyperCheckerboard<S, N> ceiling(Ray<S, N>((-e0), e0.normalize()), floorAxes);
-  // Scene<S,N> scene{&sphere, &floor, &ceiling};
-  Scene<S, N> scene{&torus, &floor, &ceiling};
+  HyperCheckerboard<S, N> ceiling(Ray<S, N>(2.5*(-e0), e0.normalize()), floorAxes);
+Scene<S,N> scene{&sphere, &floor, &ceiling};
+  //Scene<S, N> scene{&torus, &floor, &ceiling};
 
-  Camera<S, N> camera(Vec<S, N>{0, -2, -2}, // origin
+  Camera<S, N> camera(Vec<S, N>{-2., -2., -2.}, // origin
                       Vec<S, N>{},          // centre
-                      Vec<S, N>{-1},        // down
+                      Vec<S, N>{-1.,.5,0.5},        // down
                       Vec<S, N>{0, 1, -1},  // right
                       hRes, vRes);
   camera.centre = camera.origin * -.4;
@@ -789,6 +789,11 @@ template <typename S, size_t N> void render(size_t nWorkers) {
     camera.right[3 + 2 * i] = std::pow(2, -static_cast<S>(i) - 1);
     camera.down[4 + 2 * i] = std::pow(2, -static_cast<S>(i) - 1);
   }
+
+  assert( std::abs(camera.right.dot(camera.down)) < eps<S> );
+  assert( std::abs(camera.right.dot(camera.origin-camera.centre)) < eps<S> );
+  assert( std::abs(camera.down.dot(camera.origin-camera.centre)) < eps<S> );
+
 
   constexpr auto minRes = std::min(hRes, vRes);
 
@@ -822,7 +827,7 @@ int main(int argc, char *argv[]) {
     nWorkers = vm["threads"].as<size_t>();
 
   // Render!
-  render<double, 5>(nWorkers);
+  render<double, 12>(nWorkers);
   return 0;
 }
 
