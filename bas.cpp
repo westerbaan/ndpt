0eab4d80996456edb17b84674d45b96559a827e3
Bas Westerbaan <bas@westerbaan.name>
2019-03-17 15:41:09 +0100

    bas: use same parameters as bram

--- bas.cpp
+++ bas.cpp
@@ -5,14 +5,15 @@
 namespace po = boost::program_options;
 
 template <typename S, size_t N> void render(size_t nWorkers) {
-  constexpr int dpi = 300;
+  constexpr int dpi = 600;
   constexpr int hResPage = static_cast<int>(6.81102 * static_cast<double>(dpi));
 
   // Ratio of front cover is 173 : 246
   constexpr int vRes = static_cast<int>(
       (static_cast<double>(hResPage) / 173.) * 246.);
 
-  constexpr int tmp = hResPage + static_cast<int>(0.47244 * static_cast<double>(dpi));
+  constexpr int tmp = hResPage + static_cast<int>(0.551181 // 14mm in inches
+      * static_cast<double>(dpi));
   constexpr int hRes = hResPage + tmp;
   constexpr int hOffset = -tmp/2;
 
@@ -64,7 +65,10 @@ template <typename S, size_t N> void render(size_t nWorkers) {
   camera.down = camera.down.normalize() * 0.9 / hResPage;
 
   PNGScreen<S> screen(camera.hRes, camera.vRes);
-  Sampler<S, N, PNGScreen<S>> sampler(scene, camera, screen);
+  Sampler<S, N, PNGScreen<S>> sampler(scene, camera, screen,
+    0.002, // target
+    50 // minimal ray count
+  );
   sampler.nWorkers = nWorkers;
   sampler.shoot();
   screen.png.write("bas.png");
